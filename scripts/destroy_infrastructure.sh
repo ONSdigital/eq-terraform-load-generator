b1f558752e7521fa007b7f4504f92c6f563dd7c7
Ian Wootten <hi@niftydigits.com>
2019-10-24 09:10:58 +0100

    Update bucket name

--- scripts/destroy_infrastructure.sh
+++ scripts/destroy_infrastructure.sh
@@ -3,7 +3,7 @@
 set -e
 
 PROJECT_NAME=$1
-TERRAFORM_STATE_BUCKET="${TERRAFORM_STATE_BUCKET:-census-eq-terraform-tfstate}"
+TERRAFORM_STATE_BUCKET="${TERRAFORM_STATE_BUCKET:-eq-terraform-load-generator-tfstate}"
 
 terraform init --upgrade --backend-config prefix=${PROJECT_NAME} -var "project_name=${PROJECT_NAME}" --backend-config bucket=${TERRAFORM_STATE_BUCKET}
 
