e019cdcceb0d1d919ed7df6a99a2f5e8d6cdf26b
James Gardiner <james@jgardiner.co.uk>
2020-01-09 13:50:40 +0000

    Remove project from tf

--- main.tf
+++ main.tf
@@ -17,39 +17,15 @@ output "region" {
   value = var.region
 }
 
-resource "google_project" "project" {
-  name            = var.project_name
-  project_id      = random_id.id.hex
-  folder_id       = var.gcp_folder_id
-  billing_account = var.gcp_billing_account
-
-  labels = {
-    terraform = "eq-terraform-load-generator"
-    team      = var.project_team
-    env       = var.project_env
-  }
-
-  lifecycle {
-    ignore_changes = [
-      project_id,
-      name,
-    ]
-  }
-}
-
-output "google_project_id" {
-  value = google_project.project.project_id
-}
-
 resource "google_project_service" "container" {
-  project = google_project.project.project_id
+  project = var.project_name
   service = "container.googleapis.com"
 
   disable_dependent_services = true
 }
 
 resource "google_project_service" "compute" {
-  project = google_project.project.project_id
+  project = var.project_name
   service = "compute.googleapis.com"
 
   disable_dependent_services = true
@@ -57,7 +33,7 @@ resource "google_project_service" "compute" {
 }
 
 resource "google_project_service" "logging" {
-  project = google_project.project.project_id
+  project = var.project_name
   service = "logging.googleapis.com"
 
   disable_dependent_services = true
@@ -68,7 +44,7 @@ resource "google_compute_network" "k8s" {
   depends_on              = [google_project_service.compute]
   name                    = "k8s"
   auto_create_subnetworks = "true"
-  project                 = google_project.project.project_id
+  project                 = var.project_name
 }
 
 // GKE
@@ -77,11 +53,11 @@ resource "google_compute_network" "k8s" {
 resource "google_service_account" "compute" {
   account_id   = "compute"
   display_name = "Compute Engine service account"
-  project      = google_project.project.project_id
+  project      = var.project_name
 }
 
 resource "google_project_iam_member" "compute" {
-  project = google_project.project.project_id
+  project = var.project_name
   role    = "roles/editor"
   member  = "serviceAccount:${google_service_account.compute.email}"
 }
@@ -93,7 +69,7 @@ resource "google_container_cluster" "runner-benchmark" {
   min_master_version       = var.k8s_min_master_version
   initial_node_count       = 1
   remove_default_node_pool = true
-  project                  = google_project.project.project_id
+  project                  = var.project_name
   network                  = google_compute_network.k8s.self_link
 
   // Basic auth is disabled by setting user/pass to empty strings
@@ -112,7 +88,7 @@ resource "google_container_node_pool" "main-node-pool" {
   location   = var.region
   cluster    = google_container_cluster.runner-benchmark.name
   node_count = 1
-  project    = google_project.project.project_id
+  project    = var.project_name
   version    = var.k8s_min_master_version
 
   lifecycle {
@@ -148,10 +124,10 @@ resource "google_container_node_pool" "main-node-pool" {
 }
 
 resource "google_storage_bucket" "benchmark-output-storage" {
-  name          = "${google_project.project.project_id}-benchmark-outputs"
+  name          = "${var.project_name}-benchmark-outputs"
   location      = var.region
   force_destroy = "true"
-  project       = google_project.project.project_id
+  project       = var.project_name
 
   retention_policy {
     is_locked        = false
--- scripts/destroy_infrastructure.sh
+++ scripts/destroy_infrastructure.sh
@@ -8,13 +8,11 @@ tfenv use "$(< .terraform-version)"
 
 terraform init --upgrade --backend-config prefix=${TF_VAR_project_name} --backend-config bucket=${TERRAFORM_STATE_BUCKET}
 
-# Do not delete the project or bucket.
-terraform state rm google_project.project
+# Do not delete the bucket.
 terraform state rm google_storage_bucket.benchmark-output-storage
 
 # Destroy infrastructure
 terraform destroy --auto-approve
 
-# Import project and bucket resouce back into the state
-terraform import google_project.project "$TF_VAR_project_name"
+# Import bucket resouce back into the state
 terraform import google_storage_bucket.benchmark-output-storage ${TF_VAR_project_name}-benchmark-outputs
