d8e60b6cbe67e658f5d1295a131d86a3dea0fe0d
James Gardiner <james@jgardiner.co.uk>
2020-01-16 16:20:22 +0000

    Import project_id var to terraform

--- scripts/destroy_infrastructure.sh
+++ scripts/destroy_infrastructure.sh
@@ -12,7 +12,7 @@ terraform init --upgrade --backend-config prefix=${PROJECT_ID} --backend-config 
 terraform state rm google_storage_bucket.benchmark-output-storage
 
 # Destroy infrastructure
-terraform destroy --auto-approve
+terraform destroy --auto-approve -var "project_id=${PROJECT_ID}"
 
 # Import bucket resouce back into the state
 terraform import google_storage_bucket.benchmark-output-storage ${TF_VAR_project_id}-benchmark-outputs
