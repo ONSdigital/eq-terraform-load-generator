7de3232d17491f2cba6720d520e47e775804272c
James Gardiner <james@jgardiner.co.uk>
2020-01-16 16:01:20 +0000

    Remove unused vars

--- variables.tf
+++ variables.tf
@@ -14,16 +14,6 @@ variable "project_id" {
   description = "The project id in GCP"
 }
 
-variable "project_env" {
-  description = "The environment this project belongs to, for billing and reporting purposes, etc"
-  default     = "sandbox-eq"
-}
-
-variable "project_team" {
-  description = "The team this project belongs to, for billing and reporting purposes, etc"
-  default     = "eq"
-}
-
 variable "k8s_min_master_version" {
   description = "The minimum version of the master"
   default     = "1.13"
