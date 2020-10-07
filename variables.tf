79830eda742ecc22108ba3be15b0bdd66d992ab0
Mebin Abraham <35296336+MebinAbraham@users.noreply.github.com>
2020-10-07 10:01:53 +0100

    Upgrade k8s_min_master_version to 1.16 (#11)

--- variables.tf
+++ variables.tf
@@ -16,7 +16,7 @@ variable "project_id" {
 
 variable "k8s_min_master_version" {
   description = "The minimum version of the master"
-  default     = "1.15"
+  default     = "1.16"
 }
 
 variable "k8s_machine_type" {
