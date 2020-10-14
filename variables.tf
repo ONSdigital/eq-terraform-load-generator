09dde91c0b05bffd18693c6c3b6eda077b6e9944
Mebin Abraham <mebin95+work@gmail.com>
2020-10-14 11:12:48 +0100

    Remove unused variables

--- variables.tf
+++ variables.tf
@@ -2,14 +2,6 @@ variable "region" {
   default = "europe-west2"
 }
 
-variable "gcp_billing_account" {
-  description = "The billing account the project will use, defaults to the Census CTS account"
-}
-
-variable "gcp_folder_id" {
-  description = "The numeric ID of the folder this project belongs to"
-}
-
 variable "project_id" {
   description = "The project id in GCP"
 }
