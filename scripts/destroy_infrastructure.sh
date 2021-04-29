1e876c3d453b09573aeeb6e42f6f4a00c949ed9b
Andrew Maddaford <ajmaddaford@gmail.com>
2021-04-29 12:58:07 +0100

    Remove redundant 'tfenv use' command from scripts

--- scripts/destroy_infrastructure.sh
+++ scripts/destroy_infrastructure.sh
@@ -4,8 +4,6 @@ set -euxo pipefail
 
 TERRAFORM_STATE_BUCKET="${TERRAFORM_STATE_BUCKET:-eq-terraform-load-generator-tfstate}"
 
-tfenv use "$(< .terraform-version)"
-
 terraform init --upgrade --backend-config prefix=${PROJECT_ID} --backend-config bucket=${TERRAFORM_STATE_BUCKET}
 
 # Do not delete the bucket.
