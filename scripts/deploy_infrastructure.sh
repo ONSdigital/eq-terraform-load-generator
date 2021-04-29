311422cfd7d256fd15807cbab1214a469b6cd90e
Andrew Maddaford <ajmaddaford@gmail.com>
2021-04-29 15:12:18 +0100

    Update Terraform to version 0.12.31 (#13)

--- scripts/deploy_infrastructure.sh
+++ scripts/deploy_infrastructure.sh
@@ -4,7 +4,6 @@ set -euxo pipefail
 
 TERRAFORM_STATE_BUCKET="${TERRAFORM_STATE_BUCKET:-eq-terraform-load-generator-tfstate}"
 
-tfenv use "$(< .terraform-version)"
 terraform init --upgrade --backend-config prefix=${PROJECT_ID} --backend-config bucket=${TERRAFORM_STATE_BUCKET}
 
 echo "Using existing project_id: $PROJECT_ID"
