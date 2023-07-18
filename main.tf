49ddd8ca5395f12c68200b18f395972c174861e3
petechd <piotr.chudziak@ons.gov.uk>
2023-07-18 13:22:22 +0100

    Change preemtible

--- main.tf
+++ main.tf
@@ -101,6 +101,7 @@ resource "google_container_node_pool" "main-node-pool" {
 
   node_config {
     machine_type = var.k8s_machine_type
+    preemptible  = true
 
     oauth_scopes = [
       "compute-rw",
