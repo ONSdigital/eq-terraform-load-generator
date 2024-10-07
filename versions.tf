584a800b9021dd6dfe099aedb70c8646ac63b2e8
petechd <petechadwick1944@gmail.com>
2024-10-07 11:33:17 +0100

    Add updated terraform files

--- versions.tf
+++ versions.tf
@@ -1,13 +1,13 @@
 terraform {
-  required_version = ">= 1.7.3"
+  required_version = ">= 1.9.6"
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 5.31.0, < 6.0.0"
+      version = ">= 5.44.0, < 6.0.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = ">= 5.31.0, < 6.0.0"
+      version = ">= 5.44.0, < 6.0.0"
     }
     random = {
       source  = "hashicorp/random"
